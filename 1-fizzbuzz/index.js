beb790d2e215214297f286a3b368ddf4f54ae794
jennifer--MM <jenlynmenard.com>
2020-01-26 13:48:25 -0500

    fizzbuzz

--- 1-fizzbuzz/index.js
+++ 1-fizzbuzz/index.js
@@ -10,25 +10,25 @@ const getargs = require("../modules/getargs")
 let input = getargs.getIntegerArg()
 
 // test that input is a number and exit with error
-// TODO check if input is not an integer
-if ( ) {
-  // TODO provide usage statement
+
+if (isNaN(input) || !Number.isInteger(input)) {
+  console.log("usage: node 1-fizzbuzz <integer>");
 }
 else {
   // iterate from 1 to the value of input
   // TODO provide info to for loop to iterate from 1 to value of input
-  for ( ) {
+  for (let i = 1; i <= input; i++) {
     
     //TODO provide for tests for divisible for 3,5, and 15 to create desired output
-    if ( ) {
+    if (i % 15 == 0) {
       console.log(`${i} fizzbuzz`);
-    } else if ( ) {
+  } else if (i % 5 == 0) {
       console.log(`${i} fizz`);
-    } else if ( ) {
+  } else if (i % 3 == 0) {
       console.log(`${i} buzz`);
-    } else {
-      console.log(`${i}`);
-    }
+  } else {
+      console.log(`${i}`)
+  }
 
   }
 }
